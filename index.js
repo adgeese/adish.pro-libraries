a840a6d37d8a8daf03c721d988078fded67eaa2f
Brandon Wilkins <brandon@adgee.se>
2022-01-16 20:38:18 -0600

    Add central include index

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const constants = require('src/constants');
+const database = require('src/database');
+const logging = require('src/logging');
+
+exports.constants = constants;
+exports.database = database;
+exports.logging = logging;
+
