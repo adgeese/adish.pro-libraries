fc282a79e516ba02b1e1fcc7502ece8814a10d31
Brandon Wilkins <brandon@adgee.se>
2022-01-16 21:12:06 -0600

    Bug fix test yarn add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
-const constants = require('src/constants');
-const database = require('src/database');
-const logging = require('src/logging');
+const constants = require('./src/constants');
+const database = require('./src/database');
+const logging = require('./src/logging');
 
 exports.constants = constants;
 exports.database = database;
